f8c593d4efa7ae694564172a61dfad17e5113d96
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-11-06 17:37:22 -0600

    added process.env.PORT

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,9 +24,9 @@ router.put("/:id", function(request, response) {
 	burger.updateOne({devoured: true}, condition, function(result) {
 		if (result.changedRows == 0) {
 	      // If no rows were changed, then the ID must not exist, so 404
-	      return res.status(404).end();
+	      return response.status(404).end();
 	    } else {
-	      res.status(200).end();
+	      response.status(200).end();
 	    }
 	});
 });
