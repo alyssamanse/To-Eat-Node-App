f0aa34aeea55e46064639790a7d040b285807805
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-11-07 20:53:09 -0600

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// mysql://b581aefbd1fe9f:f0db50f9@us-cdbr-iron-east-05.cleardb.net/heroku_dd2237c1f9f8771?reconnect=true
-
 var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "b581aefbd1fe9f",
-  password: "f0db50f9",
-  database: "heroku_dd2237c1f9f8771"
+  host: "localhost",
+  user: "root",
+  password: "am1432",
+  database: "burgers_db"
 });
 
 // Make connection.
